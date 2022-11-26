8c3a69a7f4fc74bf0acf7530d575883bea3eb918
satyajit nayak <satyajitn15@gmail.com>
2022-11-26 16:15:15 +0530

    Add products & category repository.
    1. Add product category repository to interact with DB.
    2. Add product repository to interact with DB.

--- src/database/repository/product-category-repository.js
+++ src/database/repository/product-category-repository.js
@@ -8,84 +8,74 @@ const {
 
 class ProductCategoryRepository {
   async createProductCategory({ categoryName, categoryDetails }) {
-    try {
-      const productCategory = await ProductCategoryModel.create({
-        category_name: categoryName,
-        details: categoryDetails,
-      });
-      const { category_id, category_name, details } = productCategory;
-      return { category_id, category_name, details };
-    } catch (error) {
-      throw APIError('unable to create product category');
-    }
+    const productCategory = await ProductCategoryModel.create({
+      category_name: categoryName,
+      details: categoryDetails,
+    });
+
+    if (productCategory == null)
+      throw new NotFoundError('unable to create new category');
+
+    const { category_id, category_name, details } = productCategory;
+    return { category_id, category_name, details };
   }
 
   async getProductCategoryById({ categoryId }) {
-    try {
-      const productCategory = await ProductCategoryModel.findOne({
-        where: {
-          category_id: categoryId,
-        },
-      });
+    const productCategory = await ProductCategoryModel.findOne({
+      where: {
+        category_id: categoryId,
+      },
+    });
 
-      if (productCategory == null)
-        throw NotFoundError('unable to find the category with the given id');
+    if (productCategory == null)
+      throw new NotFoundError('unable to find the category with the given id');
 
-      const { category_id, category_name, details } = productCategory;
-      return { category_id, category_name, details };
-    } catch (error) {
-      throw APIError('unable to create product category');
-    }
+    const { category_id, category_name, details } = productCategory;
+    return { category_id, category_name, details };
   }
 
   async updateProductCategory({ categoryId, categoryName, categoryDetails }) {
-    try {
-      const updatedCategory = await ProductCategoryModel.update(
-        {
-          category_name: categoryName,
-          details: categoryDetails,
-        },
-        {
-          where: { category_id: categoryId },
-        }
-      );
+    const productCategoryExist = await ProductCategoryModel.findOne({
+      where: { category_id: categoryId },
+    });
 
-      if (updatedCategory == null)
-        throw APIError('unable to update product category');
+    if (productCategoryExist == null)
+      throw new NotFoundError('category not found');
 
-      const { category_id, category_name, details } = updatedCategory;
-      return { category_id, category_name, details };
-    } catch (error) {
-      throw APIError('unable to update product category');
-    }
+    productCategoryExist.category_name = categoryName;
+    productCategoryExist.details = categoryDetails;
+
+    const updatedCategory = await productCategoryExist.save();
+
+    if (updatedCategory == null)
+      throw new APIError('unable to update product category');
+
+    const { category_id, category_name, details } = updatedCategory;
+    return { category_id, category_name, details };
   }
 
   async deleteCategory({ categoryId }) {
-    try {
-      // check if products with same category already exists then do not delete
-      const product = await ProductCategoryModel.findOne({
-        where: {
-          category_id: categoryId,
-        },
-      });
+    // check if products with same category already exists then do not delete
+    const product = await ProductModel.findOne({
+      where: {
+        category_id: categoryId,
+      },
+    });
 
-      // since product with the given category alreday exist we can not delete it
-      if (product) throw APIError('unable to delete product category');
+    // since product with the given category alreday exist we can not delete it
+    if (product) throw new APIError('unable to delete product category');
 
-      const count = await ProductCategoryModel.destroy({
-        where: {
-          category_id: categoryId,
-        },
-      });
+    const count = await ProductCategoryModel.destroy({
+      where: {
+        category_id: categoryId,
+      },
+    });
 
-      if (count == null || count == 0) {
-        throw NotFoundError('unable to delete the category');
-      }
-
-      return true;
-    } catch (error) {
-      throw APIError('unable to delete product category');
+    if (count == null || count == 0) {
+      throw new NotFoundError('unable to delete the category');
     }
+
+    return true;
   }
 }
 
